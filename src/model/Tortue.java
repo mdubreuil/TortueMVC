2530b905b1ce29099e7b099eb9efc1c4bfe093bd
Koialle <yoite.hikari@gmail.com>
2017-05-03 17:04:02 +0200

    Refacto Tortue & segment

--- src/model/Tortue.java
+++ src/model/Tortue.java
@@ -7,5 +7,119 @@ package model;
  * @author Ophélie EOUZAN
  */
 public class Tortue {
+
+    protected static final double ratioDegRad = 0.0174533; // Rapport radians/degres (pour la conversion)
+    protected int x = 0, y = 0;
+    protected int direction = -90;
+    protected boolean crayon = true;
+    protected int couleur = 0;
+
+    public void reset() {
+        x = 0;
+        y = 0;
+        direction = -90;
+        couleur = 0;
+        crayon = true;
+    }
+
+    public void setPosition(int newX, int newY) {
+        x = newX;
+        y = newY;
+    }
+
+    public void avancer(int dist) {
+        int newX = (int) Math.round(x+dist*Math.cos(ratioDegRad * direction));
+        int newY = (int) Math.round(y+dist*Math.sin(ratioDegRad * direction));
+
+//		if (crayon==true) {
+//			Segment seg = new Segment();
+//			
+//			seg.ptStart.x = x;
+//			seg.ptStart.y = y;
+//			seg.ptEnd.x = newX;
+//			seg.ptEnd.y = newY;
+//			seg.color = decodeColor(coul);
+//	
+//			listSegments.add(seg);
+//		}
+
+        x = newX;
+        y = newY;
+    }
+
+    public void droite(int ang) {
+        direction = (direction + ang) % 360;
+    }
+
+    public void gauche(int ang) {
+        direction = (direction - ang) % 360;
+    }
+
+    public void baisserCrayon() {
+        crayon = true;
+    }
+
+    public void leverCrayon() {
+        crayon = false;
+    }
+
+    public void couleur(int n) {
+        couleur = n % 12;
+    }
+
+    public void couleurSuivante() {
+        couleur(couleur + 1);
+    }
+
+    /** quelques classiques */
+
+    public void carre() {
+        for (int i=0;i<4;i++) {
+            avancer(100);
+            droite(90);
+        }
+    }
+
+    public void poly(int n, int a) {
+        for (int j=0;j<a;j++) {
+            avancer(n);
+            droite(360/a);
+        }
+    }
+
+    public void spiral(int n, int k, int a) {
+        for (int i = 0; i < k; i++) {
+            couleur(couleur+1);
+            avancer(n);
+            droite(360/a);
+            n = n+1;
+        }
+    }
+
+    public static double getRatioDegRad() {
+        return ratioDegRad;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public boolean isCrayon() {
+        return crayon;
+    }
+
+    public int getCouleur() {
+        return couleur;
+    }
     
+    public void setColor(int n) {couleur = n;}
+    public int getColor() {return couleur;}
 }
---
+++ src/view/TortueView.java
@@ -0,0 +1,72 @@
+
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.Iterator;
+import model.Tortue;
+
+/**
+ *
+ * @author Mélanie DUBREUIL
+ * @author Ophélie EOUZAN
+ */
+public class TortueView {
+    
+    protected static final int rp = 10, rb = 5; // Taille de la pointe et de la base de la fleche
+
+    protected Tortue tortue;
+    protected ArrayList<Segment> listSegments; // Trace de la tortue
+    
+    public TortueView(Tortue tortue) {
+        this.tortue = tortue;
+        listSegments = new ArrayList();
+    }
+    
+    public void reset() {
+        tortue.reset();
+        listSegments.clear();
+    }
+    
+    public void drawTurtle (Graphics graph) {
+        if (graph == null) return;
+
+        // Dessine les segments<
+        for (Iterator it = listSegments.iterator(); it.hasNext();) {
+            Segment seg = (Segment) it.next();
+            seg.drawSegment(graph);
+        }
+
+        //Calcule les 3 coins du triangle a partir de
+        // la position de la tortue p
+        Point p = new Point(tortue.getX(),tortue.getY());
+        Polygon arrow = new Polygon();
+
+        //Calcule des deux bases
+        //Angle de la droite
+        double theta = Tortue.getRatioDegRad() * (-tortue.getDirection());
+        //Demi angle au sommet du triangle
+        double alpha = Math.atan((float) rb / (float) rp);
+        //Rayon de la fleche
+        double r = Math.sqrt(rp * rp + rb * rb);
+        //Sens de la fleche
+
+        //Pointe
+        Point p2=new Point((int) Math.round(p.x+r*Math.cos(theta)), 
+            (int) Math.round(p.y-r*Math.sin(theta)));
+        arrow.addPoint(p2.x,p2.y);
+        arrow.addPoint((int) Math.round( p2.x-r*Math.cos(theta + alpha) ),
+          (int) Math.round( p2.y+r*Math.sin(theta + alpha) ));
+
+        //Base2
+        arrow.addPoint((int) Math.round( p2.x-r*Math.cos(theta - alpha) ),
+          (int) Math.round( p2.y+r*Math.sin(theta - alpha) ));
+
+        arrow.addPoint(p2.x,p2.y);
+        graph.setColor(Color.green);
+        graph.fillPolygon(arrow);
+    }
+}
